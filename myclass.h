0b77a8b9820b1f6563983ec7e1a50e7091e65570
ericaemmanuel1 <83539543+ericaemmanuel1@users.noreply.github.com>
2022-03-03 13:36:19 +1030

    Add files via upload

--- myclass.h
+++ myclass.h
@@ -1,1 +1,27 @@
-hhhh
+#ifndef CIRCULAR_QUEUE
+#define CIRCULAR_QUEUE
+
+#include<array>
+// do not USE using namespace std!!!!
+
+class CircularQueue{
+    public:
+         void add( int frame );
+         int getNext();
+         CircularQueue( int capacity );
+         CircularQueue();
+
+    //Destructor
+    ~CircularQueue();
+
+    private:
+        int front;
+        int capacity;
+        int current_size;
+        //storing all the frames
+        std::array<int> frames;
+
+}
+
+
+#endif
