0cf01f45ebb7aead966b403c4c8ca82ae27f40c9
ericaemmanuel1 <83539543+ericaemmanuel1@users.noreply.github.com>
2022-03-03 13:38:30 +1030

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,34 @@
-#ifndef CIRCULAR_QUEUE
-#define CIRCULAR_QUEUE
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <sstream>
+#include <string>
+using namespace std;
 
-#include<array>
-// do not USE using namespace std!!!!
 
-class CircularQueue{
-    public:
-         void add( int frame );
-         int getNext();
-         CircularQueue( int capacity );
-         CircularQueue();
 
-    //Destructor
-    ~CircularQueue();
 
-    private:
-        int front;
-        int capacity;
-        int current_size;
-        //storing all the frames
-        std::array<int> frames;
+int main(){
+  //Getting the values from the input
+  vector<string> list = {};
+  string input;
+  getline(cin, input);
+  stringstream str_strm;
+  str_strm << input;
+  string temp_str;
+  while(!str_strm.eof()) {str_strm >> temp_str;list.push_back(temp_str);}
+  string a = list[0]; string b = list[1]; string B = list[2];
+  
+  int n = a.length();
+  int dif = (a.length() - b.length());
+  int kjh = b.length(); if (kjh>n){n = b.length(); dif = (b.length() - a.length());}
+  if (a.length()>b.length()){for (int i = 0; i<dif; i++) {b="0"+b;}
+  } else {for (int i = 0; i<dif; i++) {a="0"+a;}}
 
+
+  //get the sum
+  cout<<school_sum(a, b, stoi(B))<<" ";
+
+  //get the product
+  cout<<Karasuba_product(a, b, stoi(B))<<endl;
 }
-
-
-#endif
